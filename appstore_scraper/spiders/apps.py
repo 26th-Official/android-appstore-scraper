c87fff4f98100ffc9598aa3d5a761f36cbd773b7
XavierZambrano <xavierzamco@gmail.com>
2024-06-22 21:41:29 -0600

    feat: parse apps

--- appstore_scraper/spiders/apps.py
+++ appstore_scraper/spiders/apps.py
@@ -1,10 +1,28 @@
 import scrapy
+from scrapy.spiders import SitemapSpider
 
 
-class AppsSpider(scrapy.Spider):
+class AppsSpider(SitemapSpider):
     name = "apps"
     allowed_domains = ["play.google.com"]
-    start_urls = ["https://play.google.com"]
+    sitemap_urls = ['https://play.google.com/robots.txt']
+    sitemap_rules = [
+        ('/apps/', 'parse')
+    ]
 
     def parse(self, response):
-        pass
+        name = response.xpath('//h1/text()').get()
+        developer = {
+            'url': response.xpath('//div[@class="Vbfug auoIOc"]/a/@href').get(),
+            'name': response.xpath('//div[@class="Vbfug auoIOc"]/a/span/text()').get()
+        }
+        rating = response.xpath('//div[@class="jILTFe"]/text()').get()
+        about_raw = response.xpath('//div[@data-g-id="description"]//text()').getall()
+        about = ''.join(about_raw).strip()
+        yield {
+            'url': response.url,
+            'name': name,
+            'developer': developer,
+            'rating': rating,
+            'about': about
+        }
